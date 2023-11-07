a0086c761f3f7999f3db5176e7be6c0a678f0d9a
Samuel Aliyari <samuelaliyari@outlook.de>
2023-11-07 18:02:04 +0100

    Code Cleaned

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,22 +3,36 @@ const keyInput = document.body.querySelector("#keyInput");
 const buttonEncode = document.body.querySelector("#encoder");
 const buttonDecode = document.body.querySelector("#decoder");
 const output = document.body.querySelector("p");
-console.log(String.fromCharCode(97))
+
 buttonEncode.addEventListener("click", () => {
+    output.textContent = "";
+    const input = text.value.split(" ").join("");
+    const encodeKey = Number(keyInput.value);
+    for (const charachter in input) {
+        let code = input.charCodeAt(charachter) + (encodeKey - 32);
+            if (code > 90) {
+                code -= 26
+                output.textContent += String.fromCharCode(code);
+            } else {
+                output.textContent += String.fromCharCode(code);
+            }
+        }
+    }
+)
+
+buttonDecode.addEventListener("click", () => {
     output.textContent = ""
     const input = text.value.split(" ").join("");
     const encodeKey = Number(keyInput.value);
     for (const charachter in input) {
-        let code = input.charCodeAt(charachter) - 55 + encodeKey
-        output.textContent += String.fromCharCode(code)
-}}
-)
-buttonDecode.addEventListener("click", () => {
-    output.textContent = ""
-    const input = text.value;
-    const encodeKey = Number(keyInput.value);
-    for (const charachter in input) {
-        let code = input.charCodeAt(charachter) - encodeKey +55
-        output.textContent += String.fromCharCode(code)
-}}
+        let code = input.charCodeAt(charachter) + (32 - encodeKey);
+        if(code < 97){
+            code += 26
+            output.textContent += String.fromCharCode(code);
+        }
+        else {
+            output.textContent += String.fromCharCode(code);
+        }
+        }
+    }
 )
